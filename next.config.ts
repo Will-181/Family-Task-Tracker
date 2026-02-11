08909a47e206ae4389865f50036d0cc5559570cf
Will McCrea <will.mccrea@goldcoinstudios.com>
2026-02-11 14:55:58 -0800

    chore(release): v1.0.1 Change list: M	README.md M	next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  // Allow dev server to be used from other devices on your network (LAN IPs like 192.168.50.153)
+  // Next.js compares origins as host or host:port; wildcards supported.
+  allowedDevOrigins: [
+    "localhost",
+    "localhost:3000",
+    "127.0.0.1",
+    "127.0.0.1:3000",
+    "192.168.50.153",
+    "192.168.50.153:3000",
+  ],
+};
 
 export default nextConfig;
